cfd0900ee6f5e984d5953e8111f00a0077b9f37b
kazuki-0418 <kazuki.castle0418@gmail.com>
2024-11-27 23:52:14 -0800

    Exercise is done

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 $(function() {
-  // your code here
+  $(".accordion-header").on("click", function () {
+    $(".accordion-content").slideUp();
+    
+    let content = $(this).next(".accordion-content");
+    
+    if (!content.is(":visible")) {
+        content.slideDown();
+    }
+});
+
+
+$("button").on("click", function () {
+  $.ajax({
+      url: "https://dummyjson.com/todos", 
+      method: "GET",
+      success: function (response) {
+          $(".todos ul").empty();
+
+          response.todos.forEach(function (todo) {
+              $(".todos ul").append(`<li>${todo.todo}</li>`);
+          });
+      },
+      error: function () {
+          alert("Failed to load To-Do items.");
+      }
+  });
+});
 })
+
